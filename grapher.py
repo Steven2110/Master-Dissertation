1d025c20d2103ae75fc58a5ab6e33d0e65df27b5
Steven Wijaya <steven.wijaya2001@gmail.com>
2025-01-14 19:25:08 +0700

    Fix Graph

--- grapher.py
+++ grapher.py
@@ -53,18 +53,21 @@ def process_graph(subdirectory, base_path='data'):
         gaps = np.abs(np.diff(y)) > 100
         segments = np.split(np.arange(len(y)), np.where(gaps)[0] + 1)
 
+        plt.rcParams['font.family'] = 'Times New Roman'
+
         # Create a figure with subplots for stacked visualization
-        fig, axes = plt.subplots(2, 1, figsize=(14, 12), sharex=True)
+        fig, axes = plt.subplots(2, 1, figsize=(14, 12), sharex=True, gridspec_kw={'hspace': 0})
         fig.suptitle(f'Resonance Analysis for {subdirectory}', fontsize=18, y=0.95)
         
         for segment in segments:
-            axes[0].plot(x[segment], y[segment], color='black', label=f"Phi dot {i}" if segment[0] == 0 else "")
+            axes[0].plot(x[segment], y[segment]  * 1E6, color='black', label=rf"$\dot{{\phi}}_{i}$" if segment[0] == 0 else "")
 
         axes[0].set_xlabel('Time (years)', fontsize=14)
-        axes[0].set_ylabel(f'Phi Dot {i} (rad/second)', fontsize=14)
-        axes[0].set_title(f'Phi Dot {i}', fontsize=16)
+        axes[0].set_ylabel(rf'$\dot{{\Phi}}_{i}\, \text{{✕}}\, 10^{{6}}$' ' ,\n(rad/s)', fontsize=16)
+        # axes[0].set_title(rf'$\dot{{\Phi}}_{i}$', fontsize=16)
+        axes[0].yaxis.tick_right()
         axes[0].grid(True)
-        axes[0].legend()
+        # axes[0].legend(fontsize=14)
 
         # Critical arguments
         y = np.array(df[f'Phi_{i}_deg'])
@@ -75,12 +78,14 @@ def process_graph(subdirectory, base_path='data'):
 
         # Plot Phi values on the first subplot
         for segment in segments:
-            axes[1].plot(x[segment], y[segment], color='black', label=f'Phi {i}' if segment[0] == 0 else "")
+            axes[1].plot(x[segment], y[segment], color='black', label=rf'$\phi_{i}$' if segment[0] == 0 else "")
 
-        axes[1].set_ylabel(f'Phi {i} (degrees)', fontsize=14)
-        axes[1].set_title(f'Phi {i}', fontsize=16)
+        axes[1].set_yticks([120, 240, 360])
+        axes[1].set_ylabel(rf'$\Phi_{i}$' ' , (deg)', fontsize=16)
+        axes[1].set_xlabel(r'$t$, (years)', fontsize=14)
+        # axes[1].set_title(rf'$\Phi_{i}$', fontsize=16)
         axes[1].grid(True)
-        axes[1].legend()
+        # axes[1].legend(fontsize=14)
 
         # Save the combined plot
         save_path = os.path.join(save_dir, f"{subdirectory}_resonance_{i}.png")
@@ -88,4 +93,3 @@ def process_graph(subdirectory, base_path='data'):
         plt.close()
 
         print(f"Saved stacked plot for Phi and Phi Dot in {save_path}.")
-
